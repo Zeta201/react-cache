e70cfe51eac2148b569da4d8477ccd974a9f8be5
Zeta201 <dini2040gh@gmail.com>
2023-08-26 22:29:13 +0530

    countries setTimeOut

--- src/Components/Flowers.js
+++ src/Components/Flowers.js
@@ -3,36 +3,55 @@ import React, { useEffect, useState } from "react";
 import Header from "./Header";
 
 function Flowers() {
-  //   const [countryData, setCountryData] = useState(null);
-  //   const [loading, setLoading] = useState(false);
-  //   const [timer, setTimer] = useState(null);
-  //   const [isMounted, setIsMounted] = useState(false);
+  // const [countryData, setCountryData] = useState(null);
+  const [countries, setCountries] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [timer, setTimer] = useState(null);
+  const [isMounted, setIsMounted] = useState(false);
+  const countryList = [
+    {
+      name: "india",
+      fetchTime: 5000,
+    },
+    {
+      name: "pakistan",
+      fetchTime: 10000,
+    },
+    {
+      name: "china",
+      fetchTime: 50000,
+    },
+  ];
+  const fetchCountries = async (country, fetchTime) => {
+    try {
+      setLoading(true);
+      const { data } = await axios.get(
+        `https://restcountries.com/v3.1/name/${country}`
+      );
+      // setCountries(countries.filter((county) => country.name === country));
+      // await axios.get("https://restcountries.com/v3.1/name/pakistan");
+      // await axios.get("https://restcountries.com/v3.1/name/russia");
+      console.log(data);
+      // setCountryData(data);
 
-  //   const fetchCountries = async () => {
-  //     try {
-  //       setLoading(true);
-  //       const { data } = await axios.get(
-  //         "https://restcountries.com/v3.1/name/india"
-  //       );
-  //       await axios.get("https://restcountries.com/v3.1/name/pakistan");
-  //       await axios.get("https://restcountries.com/v3.1/name/russia");
-  //       console.log(data);
-  //       setCountryData(data);
-
-  //       setLoading(false);
-  //     } catch (err) {
-  //       console.log(err);
-  //       setLoading(false);
-  //     }
-  //     clearTimeout(timer);
-  //     setTimer(setTimeout(fetchCountries, 500000));
-  //   };
-  //   useEffect(() => {
-  //     if (!isMounted) {
-  //       fetchCountries();
-  //       setIsMounted(true);
-  //     }
-  //   }, []);
+      setLoading(false);
+    } catch (err) {
+      console.log(err);
+      setLoading(false);
+    }
+    clearTimeout(timer);
+    setTimer(setTimeout(() => fetchCountries(country, fetchTime), fetchTime));
+  };
+  useEffect(() => {
+    if (!isMounted) {
+      // fetchCountries();
+      for (let i = 0; i < countryList.length; i++) {
+        const { name, fetchTime } = countryList[i];
+        fetchCountries(name, fetchTime);
+      }
+      setIsMounted(true);
+    }
+  }, []);
   return (
     <>
       <Header></Header>
